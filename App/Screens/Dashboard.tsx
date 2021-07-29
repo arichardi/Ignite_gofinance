b52482734ec789ea53b1df4736c8a1bda59e5817
Andre Richardi <a.richardi@gmail.com>
2021-07-29 10:50:57 -0300

    styled components working

---
+++ App/Screens/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container, Title } from './dashboardStyles'
+
+export default function Dashboard(){
+    return (
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    )
+}
+

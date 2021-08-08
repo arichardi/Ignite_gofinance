ac2b3152f39b349d9ccc9ec2d83c4b44e4dcf3fb
arichardi <a.richardi@gmail.com>
2021-08-08 17:39:19 -0300

    botton tab working v6

--- App/routes/app.routes.tsx
+++ App/routes/app.routes.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { MaterialIcons } from '@expo/vector-icons'
+import {useTheme} from 'styled-components'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Dashboard from '../Screens/Dashboard';
@@ -8,15 +10,63 @@ const { Navigator, Screen } = createBottomTabNavigator()
 
 
 export default function AppRoutes(){
+
+    const theme = useTheme();
+
     return(
-        <Navigator>
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
             <Screen 
                 name='Listagem'
                 component={Dashboard}
+                options={{
+                    tabBarIcon: (({ size, color }) => (
+                        <MaterialIcons 
+                            name='format-list-bulleted'
+                            size={size}
+                            color={color}
+                        />
+                    )),
+                    tabBarActiveTintColor: theme.colors.secondary,
+                    tabBarLabelPosition: 'beside-icon',
+                    tabBarStyle: { height: 60 },
+                    
+                }}
             />
             <Screen 
                 name='Cadastrar'
                 component={Register}
+                options={{
+                    tabBarIcon: (({ size, color }) => (
+                        <MaterialIcons 
+                            name='attach-money'
+                            size={size}
+                            color={color}
+                        />
+                    )),
+                    tabBarActiveTintColor: theme.colors.secondary,
+                    tabBarLabelPosition: 'beside-icon',
+                    tabBarStyle: { height: 60 },
+                }}
+            />
+            <Screen 
+                name='Resumo'
+                component={Register}
+                options={{
+                    tabBarIcon: (({ size, color }) => (
+                        <MaterialIcons 
+                            name='pie-chart'
+                            size={size}
+                            color={color}
+                        />
+                    )),
+                    tabBarActiveTintColor: theme.colors.secondary,
+                    tabBarLabelPosition: 'beside-icon',
+                    tabBarStyle: { height: 60 },
+                }}
             />
         </Navigator>
     );
